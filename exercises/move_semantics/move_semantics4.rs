8b2280fb22ad091f38333b820bb807505bc343a9
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-04-07 17:33:13 -0400

    daily rustlings

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -4,12 +4,11 @@
 // freshly created vector from fill_vec to its caller.
 // Execute `rustlings hint move_semantics4` for hints!
 
-// I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let vec0 = Vec::new(); // create a new vector
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0); // vec is moved into fill_vec
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -19,8 +18,8 @@ fn main() {
 }
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
-fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec; // vec is redeclared into a mutable variable
 
     vec.push(22);
     vec.push(44);
