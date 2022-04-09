5fafa18f3e1f0f004068816bdfaba43a843bf3eb
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-04-08 23:10:09 -0400

    daily rustings types and structs

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -4,7 +4,7 @@
 // Make the code compile and the tests pass!
 // If you have issues execute `rustlings hint structs3`
 
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 struct Package {
@@ -16,7 +16,7 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("Error: Package weight cannot be less than or equal to zero grams.");
         } else {
             return Package {
                 sender_country,
@@ -26,12 +26,18 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        if self.sender_country != self.recipient_country {
+            true
+        } else {
+            false
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+        cents_per_gram * self.weight_in_grams
     }
 }
 
@@ -73,7 +79,7 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_gram = ???;
+        let cents_per_gram = 3;
 
         let package = Package::new(sender_country, recipient_country, 1500);
 
